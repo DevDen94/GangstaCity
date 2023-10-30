6a212432b0f4aebc867899d410ecbe980132140c
Shan89754 <108057549+Shan89754@users.noreply.github.com>
2023-10-30 17:48:34 +0500

    update

--- Assets/!Scripts/Gangster_Shop.cs
+++ Assets/!Scripts/Gangster_Shop.cs
@@ -7,21 +7,21 @@ public class Gangster_Shop : ScriptableObject
 {
     public string Name;
     public int Hats;
-    public int Shirts;
+    //public int Shirts;
     public int Jackets;
     public int Pants;
     public int Shoes;
     public int Glasses;
 
     public Material Hat_Material;
-    public Material Shirt_Material;
+    //public Material Shirt_Material;
     public Material Jacket_Material;
     public Material Pant_Material;
     public Material Glasses_Material;
     public Material Shoe_Material;
 
     public Sprite[] Hat_Textures;
-    public Sprite[] Shirt_Textures;
+    //public Sprite[] Shirt_Textures;
     public Sprite[] Jacket_Textures;
     public Sprite[] Pant_Textures;
     public string[] Glasses_Textures;
